efe79772f95028d096e8c81d020e8b7bf3fc86a9
sowmyashreeps76 <sowmyashree9976@gmail.com>
2025-06-08 21:26:18 +0530

    using codegen feature in playwrite and generated the scripts for login for Positive tests and negetive tests.

--- tests/codegen_login.spec.js
+++ tests/codegen_login.spec.js
@@ -1,6 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+// Possitive test case
+// Here using codegen feature in playwrite and generated the scripts for login for Positive tests and negetive tests.
+// command for that is: npx playwright codegen URL of the webpage(copy url of web page and past it here)
+
+test('Verify login with valid credentials', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/v1/');
   await page.locator('[data-test="username"]').click();
   await page.locator('[data-test="username"]').fill('standard_user');
@@ -8,4 +12,36 @@ test('test', async ({ page }) => {
   await page.locator('[data-test="password"]').fill('secret_sauce');
   await page.getByRole('button', { name: 'LOGIN' }).click();
   await expect(page.getByText('Products')).toBeVisible();
+});
+
+// Negetive test cases
+
+test('Verify login with valid username and Invalid password', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/v1/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('hgdjhgfisg');
+  await page.getByRole('button', { name: 'LOGIN' }).click();
+  await expect(page.locator('[data-test="error"]')).toBeVisible();
+});
+
+test('Verify login with Invalid username and valid password', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/v1/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('65ytyty');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.getByRole('button', { name: 'LOGIN' }).click();
+  await expect(page.locator('[data-test="error"]')).toBeVisible();
+});
+
+test('Verify login with Invalid username and Invalid password', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/v1/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('fdfgregf');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('dfdsfd');
+  await page.getByRole('button', { name: 'LOGIN' }).click();
+  await expect(page.locator('[data-test="error"]')).toBeVisible();
 });
